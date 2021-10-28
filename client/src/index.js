07e83d706d7a5b86a7d3dcf99c67ae77b851b63e
Leandro Lima <leandro.lima@oowlish.com>
2021-10-28 17:58:51 -0300

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001',
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { useQuery, gql } from '@apollo/client';
 import { Layout } from '../components';
+import TrackCard from '../containers/track-card';
+import { QueryResult } from '../components';
+
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} data={data} loading={loading}>
+        {data?.tracksForHome?.map(track => (
+          <TrackCard track={track}/>
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
